80dd48f59e7f7b0699420996b4f7a434e1533105
Gautamverma2003 <gautam146official@gmail.com>
2025-01-10 00:48:01 +0530

    DivisibilityCheck

---
+++ day-2/level-1/ques1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ques1 {
+    public static void main(String[] args) {
+        // Create a Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Check if the number is divisible by 5
+        if (number % 5 == 0) {
+            System.out.println("Is the number " + number + " divisible by 5? Yes");
+        } else {
+            System.out.println("Is the number " + number + " divisible by 5? No");
+        }
+
+        
+    }
+}

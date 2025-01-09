f96a0c1ed729d1631cf2cccd86e8b130fb2526ab
Gautamverma2003 <gautam146official@gmail.com>
2025-01-09 22:20:18 +0530

    double Opt added

--- day-1/level-2/ques3.java
+++ day-1/level-2/ques3.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
-public class ques1 {
+public class ques3 {
     public static void main(String[] args) {
      
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
  System.out.println("enter first number");
- int a = sc.nextInt();
+ double a = sc.nextDouble();
   System.out.println("enter second number");
- int  b= sc.nextInt();
+ double  b= sc.nextDouble();
   System.out.println("enter third number");
- int c = sc.nextInt();
-  int operation1 = a + b *c;
-  int operation2 = a * b + c;
-  int operation3 = c + a / b;
-  int operation4 = a % b + c;
+ double c = sc.nextDouble();
+  double operation1 = a + b *c;
+  double operation2 = a * b + c;
+  double operation3 = c + a / b;
+  double operation4 = a % b + c;
   
   System.out.println("The results of Int Operations are " + operation1 + ", " + operation2 +"," + operation3 + "and " + operation4);
   }

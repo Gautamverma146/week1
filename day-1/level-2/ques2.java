9763a279b043f6b9e5397bc945c57b5054aaa5ee
Gautamverma2003 <gautam146official@gmail.com>
2025-01-09 21:58:25 +0530

    added some l2

---
+++ day-1/level-2/ques2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ques1 {
+    public static void main(String[] args) {
+     
+        Scanner scanner = new Scanner(System.in);
+ System.out.println("enter first number");
+ int a = sc.nextInt();
+  System.out.println("enter second number");
+ int  b= sc.nextInt();
+  System.out.println("enter third number");
+ int c = sc.nextInt();
+  int operation1 = a + b *c;
+  int operation2 = a * b + c;
+  int operation3 = c + a / b;
+  int operation4 = a % b + c;
+  
+  System.out.println("The results of Int Operations are " + operation1 + ", " + operation2 +"," + operation3 + "and " + operation4);
+  }
+  }
+  

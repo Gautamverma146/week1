f1bd79812cc9e74476ae33e2b4331487187c1531
Gautamverma2003 <gautam146official@gmail.com>
2025-01-10 10:29:45 +0530

    GradeCalculator

---
+++ day-2/level-2/ques3.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class ques3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+       
+        System.out.print("Enter marks in Physics: ");
+        double physics = scanner.nextDouble();
+
+        System.out.print("Enter marks in Chemistry: ");
+        double chemistry = scanner.nextDouble();
+
+        System.out.print("Enter marks in Maths: ");
+        double maths = scanner.nextDouble();
+
+        
+        double totalMarks = physics + chemistry + maths;
+        double percentage = (totalMarks / 300) * 100;
+
+        
+        String grade, remarks;
+
+        if (percentage >= 80) {
+            grade = "A";
+            remarks = "Level 4, above agency-normalized standards";
+        } else if (percentage >= 70) {
+            grade = "B";
+            remarks = "Level 3, at agency-normalized standards";
+        } else if (percentage >= 60) {
+            grade = "C";
+            remarks = "Level 2, below but approaching agency-normalized standards";
+        } else if (percentage >= 50) {
+            grade = "D";
+            remarks = "Level 1, well below agency-normalized standards";
+        } else if (percentage >= 40) {
+            grade = "E";
+            remarks = "Level 1-, too below agency-normalized standards";
+        } else {
+            grade = "R";
+            remarks = "Remedial standards";
+        }
+
+       
+        System.out.printf("Average Percentage: %.2f%%\n", percentage);
+        System.out.println("Grade: " + grade);
+        System.out.println("Remarks: " + remarks);
+
+        
+    }
+}

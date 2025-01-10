a5b77785a96e0c3c2510b486ad4c9147f3d49f99
Gautamverma2003 <gautam146official@gmail.com>
2025-01-10 10:16:26 +0530

    LeapYearChecker

---
+++ day-2/level-2/ques1.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class ques1 {
+    public static void main(String[] args) {
+       
+    Scanner scanner = new Scanner(System.in);
+
+        
+    System.out.print("Enter a year (>= 1582): ");
+     int year = scanner.nextInt();
+
+      // Check if the year is valid 
+       if (year < 1582) {
+         System.out.println("Invalid input! The Gregorian calendar started in 1582.");
+      } else {
+   
+      System.out.println("\n--- Using Multiple if-else Statements ---");
+      if (year % 4 != 0) {
+            System.out.println("The year " + year + " is NOT a Leap Year.");
+            } else if (year % 100 != 0) {
+                System.out.println("The year " + year + " IS a Leap Year.");
+            } else if (year % 400 == 0) {
+                System.out.println("The year " + year + " IS a Leap Year.");
+            } else {
+                System.out.println("The year " + year + " is NOT a Leap Year.");
+            }
+
+          
+            
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                System.out.println("The year " + year + " IS a Leap Year.");
+            } else {
+                System.out.println("The year " + year + " is NOT a Leap Year.");
+            }
+        }
+
+       
+    }
+}

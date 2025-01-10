680be27c37acfacade12b6412799b1a5efdef57c
Gautamverma2003 <gautam146official@gmail.com>
2025-01-10 10:37:41 +0530

    PrimeNumberCheck

---
+++ day-2/level-2/ques4.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ques4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Taking input from the user
+        System.out.print("Enter a number to check if it's a prime number: ");
+        int number = scanner.nextInt();
+
+        boolean isPrime = true;  // Initialize as true
+
+        // Prime number check
+        if (number <= 1) {
+            isPrime = false;  // Numbers less than or equal to 1 are not prime
+        } else {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;  // Number is divisible by another number
+                    break;  // Exit loop early if not prime
+                }
+            }
+        }
+
+        
+        if (isPrime) {
+            System.out.println(number + " is a Prime Number.");
+        } else {
+            System.out.println(number + " is NOT a Prime Number.");
+        }
+
+        
+    }
+}
